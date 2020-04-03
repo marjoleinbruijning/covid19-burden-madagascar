4723055c99c6adfcb373b7d76ff1c3acdf4f4e51
marjoleinbruijning <marjoleinbruijning@gmail.com>
2020-04-03 12:03:58 -0400

    Update script.R

--- script.R
+++ script.R
@@ -4,80 +4,112 @@
 ############################ 03-31-2020 ########################################
 ################################################################################
 
-setwd('/home/mbruijning/Dropbox/Work/Manuscripts/Covid19Madagascar/')
+setwd('/home/mbruijning/Dropbox/Work/Manuscripts/Covid19Africa/')
 
 # load packages
 require(raster)
 require(rgdal)
 
-################################################################################
-##### Without age structure (just to test/explore)
-# Load demographic data (combining all ages) (data from: https://www.worldpop.org/geodata/summary?id=6396)
-mat <- raster('Data/mdg_ppp_2020.tif')
-
-# Create new map with decreased resolution
-decrReso <- 20
-mat <- aggregate(mat,fact=decrReso,fun=sum)
-
-# Plot results
-plot(mat)
-plot(log(mat))
-
-# Mutiply with infection probability and overall mortality rate (currently just arbitary numbers)
-pinf <- 0.4
-pmort <- 0.05
-plot(mat * pinf * pmort)
-
 
 ################################################################################
-##### Including age structure
-# load all datas (all datasets from https://www.worldpop.org/geodata/summary?id=16870)
-# So far only including women!
-allfiles <- as.list(list.files('Data/AgeSpecific',full.names=TRUE))
+# Load data
+allfiles <- as.list(list.files('wp_data/africa_10km_2020',full.names=TRUE))
+
+## Use Mal's maps and combine into one brick file
 dat <- lapply(allfiles,raster)
+namess <- lapply(dat,names) # file names
+uniquenames <- substr(namess,6,21) # exl gender
 
-# Decrease resolution (takes a while)
-decrReso <- 10
-dat <- lapply (dat,function(x) {
-  mat <- aggregate(x,fact=decrReso,fun=sum)
-  cat('Done \n')
-  return(mat)
-} )
+# Sum M + F
+datsubs <- list()
+for (i in 1:14) {
+  datsubs[[i]] <- dat[[i]] + dat[[which(uniquenames == uniquenames[i])[2]]]
+}
 
-# get age classes from file names
-ageclasses <- unlist(lapply(dat,function(x) {
-  a <- names(x)
-  sub(".*_f_ *(.*?) *_.*", "\\1", a)
-}))
+agelower <- substr(namess[1:14],8,9)
+ageupper <- substr(namess[1:14],10,11)
+ageclasses <- as.numeric(agelower) + 2
+names(datsubs) <- paste(agelower,ageupper)
 
-# arbitrary linear age-specific mortalility (just to test! Replace with real estimates)
-pmort <- matrix(NA,nrow=81,ncol=2)
-colnames(pmort) <- c('Age','Mortality')
-pmort[,1] <- 0:80
-pmort[,2] <- 0.01 + pmort[,1]/100
-pmortInc <- pmort[match(ageclasses,pmort[,1]),2] # age-specific mortality per file
-
-# Numbers from: https://www.imperial.ac.uk/media/imperial-college/medicine/sph/ide/gida-fellowships/Imperial-College-COVID19-NPI-modelling-16-03-2020.pdf
-# Table 1, column Infection Fatality Ratio
-pmort <- matrix(ncol=3,nrow=9)
-colnames(pmort) <- c('minage','maxage','fatality')
-pmort[,1] <- seq(0,80,10)
-pmort[,2] <- c(seq(9,80,10),120)
-pmort[,3] <- c(0.00002,0.00006,0.0003,0.0008,0.0015,0.006,0.022,0.051,0.093)
-# age-specific mortality per map
-pmortInc <- sapply(as.numeric(ageclasses),function(x)
-                   pmort[data.table::between(x,pmort[,1],pmort[,2]),3])
-names(pmortInc) <- as.numeric(ageclasses)
+dat <- brick(datsubs)
+# save output
+writeRaster(dat, filename='demoMapAfrica2020.tif', format="GTiff",
+             overwrite=TRUE,options=c("INTERLEAVE=BAND","COMPRESS=LZW"))
 
 
-# Infection probability
-pinf <- 0.4
+########################################################
+## Run analysis and create plots 
+## (functions/calculations used for Shinyapp)
+########################################################
 
-# Multiply age-specific pop density with age-specific mortality and infection probability
-finalMap <- mapply(function(x,y) x*y*pinf,dat,pmortInc)
-#plot(stack(finalMap)) # plot all maps
-plot(finalMap[[1]]) # or just one of the maps
+#########################
+## Load data and input
+dat <- brick('demoMapAfrica2020.tif')
+admin0 <- readOGR("shapefiles/country.shp")
+admin1 <- readOGR("shapefiles/admin1.shp")
 
-# Sum over age classes
-sumMap <- Reduce('+', finalMap) # Total burden
-plot(sumMap) ## Plot final burden
+## Calculate CFR per age class
+age.upper <- c(9, 19, 29, 39, 49, 59, 69, 79, 89)
+N.cases <- c(416, 549, 3619, 7600, 8571, 10008, 8583, 3918, 1408)
+N.deaths <- c(0, 1, 7, 18, 38, 130, 309, 312, 208)
+CFR <- N.deaths/N.cases
+fit.cfr <- smooth.spline(age.upper - 4.5, (N.deaths/ N.cases)) # mid-point of age bracket & cases/deaths
+
+## age classes from data
+# (dput from matrix created above)
+ageclasses <- structure(c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60,
+                              65, 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59, 64, NA), .Dim = c(14L,
+                              2L))
+ageclasses <- matrix(NA,ncol=2,nrow=length(agelower))
+ageclasses[,1] <- as.numeric(agelower)
+ageclasses[,2] <- as.numeric(ageupper)
+cfr <- pmax(predict(fit.cfr, ageclasses[,1] + 2.5)$y, 0) ## predict age-specific mortality
+
+countries <- c("All","Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi",
+                   "Cameroon", "Cape Verde", "Central African Republic", "Chad",
+                   "Comoros", "Congo", "Cte d'Ivoire", "Democratic Republic of the Congo",
+                   "Djibouti", "Egypt", "Equatorial Guinea", "Eritrea", "Ethiopia",
+                   "Gabon", "Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Kenya",
+                   "Lesotho", "Liberia", "Libya", "Madagascar", "Malawi", "Mali",
+                   "Mauritania", "Mauritius", "Mayotte", "Morocco", "Mozambique",
+                   "Namibia", "Niger", "Nigeria", "Runion", "Rwanda", "Saint Helena",
+                   "Sao Tome And Principe", "Senegal", "Seychelles", "Sierra Leone",
+                   "Somalia", "South Africa", "South Sudan", "Sudan", "Swaziland",
+                   "Tanzania", "Togo", "Tunisia", "Uganda", "Western Sahara", "Zambia",
+                   "Zimbabwe")
+
+#########################
+## Do calculations
+## User input
+input <- list()
+input$age <- 99 # corresponds to rows in matrix ageclasses; 99 for 'all' ageclasses
+input$country <- 1 # corresponds to vector countries
+input$pinf <- 0.4 # cumulative infection
+input$log <- TRUE
+
+countryname <- countries[as.numeric(input$country)]
+if (countryname == 'All') {
+  countrysubs <- dat
+} else {
+  countrysubs <- mask(dat, admin1[admin1$name_0 == countryname,])
+  countrysubs <- crop(countrysubs,admin1[admin1$name_0 == countryname,])
+}
+
+if (input$age == '99') {  # if all age classes
+  y <- countrysubs * cfr * input$pinf # multiply each age class with cfr
+  y <- calc(y,sum)
+
+} else { # for one age class
+  y <- countrysubs[[as.numeric(input$age)]] * input$pinf * cfr[as.numeric(input$age)]
+}
+
+if (input$log == TRUE) {y <- log10(y)}
+
+# create plot
+cols <- c('#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026')
+plot(y,
+    main=paste0('Country overview: ',countryname),
+    bty='n',xaxs="i", yaxs="i",box=FALSE,col=cols,xlab='X',ylab='Y')
+ plot(admin1[admin1$name_0 == countryname,],add=TRUE)
+ plot(admin0,add=TRUE)
+
